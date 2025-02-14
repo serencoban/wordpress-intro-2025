63f0521bf4fdbdaca7672a989c225e09ed64aa1c
seren <seren.coban@student.hepl.be>
2025-02-14 09:10:45 +0100

    commit

---
+++ wp-content/themes/dw/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= wp_title('.',false, 'right') . get_bloginfo('name') ?></title>  <!-- display false en gros -->
+</head>
+
+<body>
+    <h1><?= get_bloginfo('name') ?></h1>
+    <p><?= get_bloginfo('description') ?></p>
+</body>
+
+</html>
